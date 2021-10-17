ea7393cc1ea0b09a579be1e3bdd25cf7899bae06
yoons <alohalo98@naver.com>
2021-10-17 17:14:41 +0900

    Updates

---
+++ precache-manifest.ace4fca0d105140bf7f6e7d19b2a346a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7df7e9b6e3c5a4c320120787d23544",
+    "url": "/iiolo/tsTodolist.git/index.html"
+  },
+  {
+    "revision": "f39930df6d5b9ec18447",
+    "url": "/iiolo/tsTodolist.git/static/css/main.efd309cd.chunk.css"
+  },
+  {
+    "revision": "e96d7a727a7469053204",
+    "url": "/iiolo/tsTodolist.git/static/js/2.c70a6a16.chunk.js"
+  },
+  {
+    "revision": "f39930df6d5b9ec18447",
+    "url": "/iiolo/tsTodolist.git/static/js/main.74de6332.chunk.js"
+  },
+  {
+    "revision": "6c6537f6cb3a5740c6b3",
+    "url": "/iiolo/tsTodolist.git/static/js/runtime~main.8d3110d6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/iiolo/tsTodolist.git/precache-manifest.ace4fca0d105140bf7f6e7d19b2a346a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iiolo/tsTodolist.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
